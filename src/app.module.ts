7b318727d704a7a71ba407c84b88041869783eb2
Stanislav Korchevskyi <stanislav.codes@gmail.com>
2023-05-30 16:09:22 +0300

    'initial setup'

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { PrismaService } from './prisma.service'
+
+@Module({
+  controllers: [AppController],
+  providers: [PrismaService],
+})
+export class AppModule {}
